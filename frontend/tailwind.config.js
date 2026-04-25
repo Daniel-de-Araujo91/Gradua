18340f69cf0d3618c673325501d81ba3fe3b8be6
Daniel de Araujo Albuquerque <daa@ic.ufal.br>
2026-04-25 18:30:16 +0000

    color change (色の変化)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,16 +6,17 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/flowbite-react/lib/esm/**/*.js",
-    ".flowbite-react\\class-list.json"
+    ".flowbite-react\\class-list.json",
+    ".flowbite-react/class-list.json"
   ],
   theme: {
     extend: {
       colors: {
         gradua: {
           inicio: '#1e3a5f',
-          agenda: '#6d597a',
-          forum: '#52796f',
-          perfil: '#b85d43'
+          agenda: '#85305a',
+          forum: ' #1e5f22',
+          perfil: '#c26d23'
         }
       }
     },
