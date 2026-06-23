6ff26b4f2bd87562ae8b8e386b6d58e1c75be497
Daniel-de-Araujo91 <daa@ic.ufal.br>
2026-06-23 01:56:37 -0300

    Fix: Tb_Attendance

--- backend/src/main/java/br/com/ufal/gradua/models/agenda/SessionAttendanceModel.java
+++ backend/src/main/java/br/com/ufal/gradua/models/agenda/SessionAttendanceModel.java
@@ -19,7 +19,7 @@ import jakarta.persistence.FetchType;
 @Entity
 @Table(name = "TB_ATTENDANCE")
 public class SessionAttendanceModel implements Serializable {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L ;
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
